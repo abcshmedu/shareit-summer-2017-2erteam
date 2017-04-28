d9136b5109547393ec03c9abb04976f350b9440a
Thomas Großbeck <grossbec@hm.edu>
2017-04-28 22:08:11 +0200

    MediaServiceImplTest an isbn13 angepasst

--- src/test/java/geschaeftslogikTest/MediaServiceImplTest.java
+++ src/test/java/geschaeftslogikTest/MediaServiceImplTest.java
@@ -24,13 +24,13 @@ public class MediaServiceImplTest {
     private MediaService media;
     
     private Book[] books = {
-            new Book("a", "a", "1"),
-            new Book("b", "b", "2")
+        new Book("a", "a", "978-1-4028-9462-6"),
+        new Book("b", "b", "978-3-16-148410-0")
     };
     
     private Disc[] discs = {
-       new Disc("TestTitel", "1234", 0, "TestDirector"),
-       new Disc("TestTitel2", "5678", 0, "TestDirector")
+        new Disc("TestTitel", "9781402894626", 0, "TestDirector"),
+        new Disc("TestTitel2", "9783161484100", 0, "TestDirector")
     };
     
     @Before
@@ -42,27 +42,27 @@ public class MediaServiceImplTest {
 //******************************************************************************************
     @Test
     public void testAddDiscOk() {
-        MediaServiceResult res = media.addDisc(new Disc("TestTitel", "12345", 0, "TestDirector"));
+        MediaServiceResult res = media.addDisc(new Disc("TestTitel", "9783826690211", 0, "TestDirector"));
         assertEquals(res.getCode(), Status.OK.getStatusCode());
     }
     
     @Test 
     public void testAddDiscConflict() {
-        MediaServiceResult res = media.addDisc(new Disc("TestTitel", "12345", 0, "TestDirector"));
-        MediaServiceResult res2 = media.addDisc(new Disc("TestTitel", "12345", 0, "TestDirector"));
+        MediaServiceResult res = media.addDisc(new Disc("TestTitel", "9783826690211", 0, "TestDirector"));
+        MediaServiceResult res2 = media.addDisc(new Disc("TestTitel", "9783826690211", 0, "TestDirector"));
         assertEquals(res2.getCode(), Status.CONFLICT.getStatusCode());
     }
     
     @Test
     public void testAddDiscBadRequest() {
-        MediaServiceResult res = media.addDisc(new Disc("", "1234", 0, ""));
+        MediaServiceResult res = media.addDisc(new Disc("", "9783826690211", 0, ""));
         assertEquals(res.getCode(), Status.BAD_REQUEST.getStatusCode());
     }
     
     @Test
     public void testGetDiscs() {
-        Disc disc1 = new Disc("TestTitel1", "1234", 0, "TestDirector");
-        Disc disc2 = new Disc("TestTitel2", "5678", 0, "TestDirector");
+        Disc disc1 = new Disc("TestTitel1", "9781402894626", 0, "TestDirector");
+        Disc disc2 = new Disc("TestTitel2", "9783161484100", 0, "TestDirector");
         media.addDisc(disc1);
         media.addDisc(disc2);
         assertEquals(media.getDiscs(), discs);
@@ -70,52 +70,52 @@ public class MediaServiceImplTest {
     
     @Test
     public void testGetDisc() {
-        assertEquals(media.getDisc("1234"), discs[0]);
-        assertEquals(media.getDisc("9012"), null);
+        assertEquals(media.getDisc("9781402894626"), discs[0]);
+        assertEquals(media.getDisc("9783806875119"), null);
     }
     
     @Test
     public void testUpdateDiscOk() {
-        MediaServiceResult res = media.updateDisc("1234", new Disc("NewTitle", "1234", 0, "NewDirector"));
+        MediaServiceResult res = media.updateDisc("9781402894626", new Disc("NewTitle", "9781402894626", 0, "NewDirector"));
         assertEquals(res.getCode(), Status.OK.getStatusCode());
     }
     
     @Test
     public void testUpdateDiscNotFound() {
-        MediaServiceResult res = media.updateDisc("9012", new Disc("NewTitle", "1234", 0, "NewDirector"));
+        MediaServiceResult res = media.updateDisc("9783806875119", new Disc("NewTitle", "9781402894626", 0, "NewDirector"));
         assertEquals(res.getCode(), Status.NOT_FOUND.getStatusCode());
     }
     
     @Test
     public void testUpdateDiscBadRequest() {
-        MediaServiceResult res = media.updateDisc("1234", new Disc("", "1234", 0, ""));
+        MediaServiceResult res = media.updateDisc("9781402894626", new Disc("", "9781402894625", 0, ""));
         assertEquals(res.getCode(), Status.BAD_REQUEST.getStatusCode());
     }
 //Books
 //**************************************************************************************
     @Test
     public void testAddBookOk() {
-        MediaServiceResult res = media.addBook(new Book("TestTitel", "TestAutor", "12345"));
+        MediaServiceResult res = media.addBook(new Book("TestTitel", "TestAutor", "978-3-8068-7511-9"));
         assertEquals(res.getCode(), Status.OK.getStatusCode());
     }
     
     @Test 
     public void testAddBookConflict() {
-        MediaServiceResult res = media.addBook(new Book("TestTitel", "TestAutor", "1234"));
-        MediaServiceResult res2 = media.addBook(new Book("TestTitel", "TestAutor", "1234"));
+        MediaServiceResult res = media.addBook(new Book("TestTitel", "TestAutor", "978-3-8266-9021-1"));
+        MediaServiceResult res2 = media.addBook(new Book("TestTitel", "TestAutor", "978-3-8266-9021-1"));
         assertEquals(res2.getCode(), Status.CONFLICT.getStatusCode());
     }
     
     @Test
     public void testAddBookBadRequest() {
-        MediaServiceResult res = media.addBook(new Book("", "", "1234"));
+        MediaServiceResult res = media.addBook(new Book("", "", "978-3-8266-9021-1"));
         assertEquals(res.getCode(), Status.BAD_REQUEST.getStatusCode());
     }
     
     @Test
     public void testGetBooks() {
-        Book book1 = new Book("a", "a", "1");
-        Book book2 = new Book("b", "b", "2");
+        Book book1 = new Book("a", "a", "978-1-4028-9462-6");
+        Book book2 = new Book("b", "b", "978-3-16-148410-0");
         media.addBook(book1);
         media.addBook(book2);
         assertEquals(media.getBooks(), books);
@@ -123,25 +123,25 @@ public class MediaServiceImplTest {
     
     @Test
     public void testGetBook() {
-        assertEquals(media.getBook("1"), books[0]);
-        assertEquals(media.getBook("3"), null);
+        assertEquals(media.getBook("978-1-4028-9462-6"), books[0]);
+        assertEquals(media.getBook("978-3-8068-7511-9"), null);
     }
     
     @Test
     public void testUpdateBookOk() {
-        MediaServiceResult res = media.updateBook("1", new Book("NewTitle", "NewAutor", "1"));
+        MediaServiceResult res = media.updateBook("978-1-4028-9462-6", new Book("NewTitle", "NewAutor", "978-1-4028-9462-6"));
         assertEquals(res.getCode(), Status.OK.getStatusCode());
     }
     
     @Test
     public void testUpdateBookNotFound() {
-        MediaServiceResult res = media.updateBook("3", new Book("NewTitle", "NewAutor", "3"));
+        MediaServiceResult res = media.updateBook("78-3-8266-9021-1", new Book("NewTitle", "NewAutor", "78-3-8266-9021-1"));
         assertEquals(res.getCode(), Status.NOT_FOUND.getStatusCode());
     }
     
     @Test
     public void testUpdateBookBadRequest() {
-        MediaServiceResult res = media.updateBook("1", new Book("", "", "1"));
+        MediaServiceResult res = media.updateBook("978-1-4028-9462-6", new Book("", "", "978-1-4028-9462-6"));
         assertEquals(res.getCode(), Status.BAD_REQUEST.getStatusCode());
     }
 }
